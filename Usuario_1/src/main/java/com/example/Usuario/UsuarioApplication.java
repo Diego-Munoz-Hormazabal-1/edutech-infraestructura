1f5b6a4670f3e631c0c30ce10629ca433bd97ad2
DiegMuñozH <dieg.munozh@duocuc.cl>
2026-07-17 00:18:56 -0400

    implementando LAMBDA

--- Usuario_1/src/main/java/com/example/Usuario/UsuarioApplication.java
+++ Usuario_1/src/main/java/com/example/Usuario/UsuarioApplication.java
@@ -1,12 +1,15 @@
 package com.example.Usuario;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class UsuarioApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UsuarioApplication.class, args);
+        new SpringApplicationBuilder(UsuarioApplication.class)
+                .web(WebApplicationType.SERVLET) // Fuerza el modo Servlet tradicional para que AWS Lambda funcione impecable
+                .run(args);
     }
 }
